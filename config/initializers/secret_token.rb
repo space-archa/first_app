9d962470f293303daefff086a600390f2d78dc30
Artem <archa.sync@gmail.com>
2012-02-16 16:54:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'cfad15f04780726803f89557d2266e96a4ea41e0b2ea0e30174e6268c51a5a47a97722052aac750dac2f50b711f425e660f7c02b3f757b3dd55e443874c14d50'
